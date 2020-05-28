9aae37ccb47e67af99c1f93a0d00c41415245fc1
olbol <olbolot@gmail.com>
2020-05-28 14:33:07 +0300

    Add translations

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
+const translate = (string, lang) => {
+  const translations = {
+    en: {
+      'В ночь с': 'on the night of',
+      'на': 'to',
+    },
+    ru: {
+      'В ночь с': 'В ночь с',
+      'на': 'на',
+    },
+    de: {
+      'В ночь с': 'in der Nacht vom ',
+      'на': 'auf den',
+    },
+    cs: {
+      'В ночь с': 'V noci od',
+      'на': 'do',
+    }
+  };
+
+  return translations[lang][string];
+}
+
 module.exports = {
-  renderTime: date => {
+  /* Функция возвращает время в нужном формате. */
+  renderTime: ( date, lang ) => {
     if (!date) return;
-    return date.toLocaleTimeString('en', {hour: '2-digit', minute: '2-digit'});
+    return date.toLocaleTimeString(lang, {hour: '2-digit', minute: '2-digit'});
   },
-   renderDate: date => {
+
+  /* Функция возвращает дату в нужном формате. */
+   renderDate: ( date, lang ) => {
     if (!date) return;
 
+     const local = {
+       'en': 'en-US',
+       'de': 'de-DE',
+       'cs': 'cs-CS',
+       'ru': 'ru-RU',
+     };
     const hours = date.getHours();
     const optionsWithoutYear = {day: 'numeric', month: 'long'};
     const options = {year: 'numeric', month: 'long', day: 'numeric'};
@@ -15,18 +47,18 @@ module.exports = {
     if (hours > 21) {
       const nextDay = newDay.setDate(date.getDate() + 1);
 
-      return `On the night from
-        ${new Intl.DateTimeFormat('en-US', optionsWithoutYear).format(date)}
-        to
-        ${new Intl.DateTimeFormat('en-US', options).format(nextDay)}`;
+      return `${translate('В ночь с', lang)}
+        ${new Intl.DateTimeFormat(local[lang], optionsWithoutYear).format(date)}
+        ${translate('на', lang)}
+        ${new Intl.DateTimeFormat(local[lang], options).format(nextDay)}`;
     } else if (hours < 4 || hours === '0') {
       const prevDay = newDay.setDate(date.getDate() - 1);
 
-      return `On the night from
-        ${new Intl.DateTimeFormat('en-US', optionsWithoutYear).format(prevDay)}
-        to
-        ${new Intl.DateTimeFormat('en-US', options).format(date)}`;
+      return `$${translate('В ночь с', lang)}
+        ${new Intl.DateTimeFormat(local[lang], optionsWithoutYear).format(prevDay)}
+       ${translate('на', lang)}
+        ${new Intl.DateTimeFormat(local[lang], options).format(date)}`;
     }
-    return new Intl.DateTimeFormat('en-US', options).format(date);
+    return new Intl.DateTimeFormat(local[lang], options).format(date);
   }
-}
+};
